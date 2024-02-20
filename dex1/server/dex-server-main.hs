08828842a1ff796a0820f5e3ab6e6d8ea7a98038
minolinpwork <minolinp.work@gmail.com>
2024-02-20 05:33:58 +0000

    port parameter working

--- dex1/server/dex-server-main.hs
+++ dex1/server/dex-server-main.hs
@@ -1,7 +1,7 @@
 import           Control.Exception           (try)
 import           Control.Monad.Trans.Except
 import qualified Network.HTTP.Types          as HttpTypes
-import           Network.Wai.Handler.Warp    (run)
+import           Network.Wai.Handler.Warp    (run, Port)
 import           Network.Wai.Middleware.Cors
 import           Servant
 import           System.Environment          (getArgs)
@@ -12,14 +12,17 @@ import           Dex.Api.Api
 import           Dex.Api.Context
 import           Data.Aeson.Encode.Pretty    (encodePretty)
 import qualified Data.ByteString.Lazy.Char8  as BL8
-import           Prelude                     hiding (read)
+import           Prelude                     
 
 -- | Getting path for our core configuration.
-parseArgs :: IO FilePath
+parseArgs :: IO (FilePath, Int)
 parseArgs = do
   args <- getArgs
+  putStrLn $ "args: " <> show args
   case args of
-    coreCfg: _ -> return coreCfg
+    coreCfg: port: _ -> return (coreCfg, read port :: Int)
+    coreCfg: _ -> return (coreCfg, 8081)
+    --[coreCfg: port]: _ -> return (coreCfg, port)
     _invalidArgument -> fail "Error: wrong arguments, needed a path to the CoreConfig JSON configuration file\n"
 
 main :: IO ()
@@ -28,12 +31,12 @@ main = do
   BL8.writeFile "swagger-api.json" (encodePretty apiSwagger)
 
   putStrLn "parsing Config ..."
-  coreCfgPath <- parseArgs
+  (coreCfgPath, port) <- parseArgs
   coreCfg <- coreConfigIO coreCfgPath  -- Parsing our core configuration.
 
   putStrLn "Loading Providers ..."
   withCfgProviders coreCfg "api-server"  $ \providers -> do
-    let port = 8081
+    let -- port = portS
         ctx = Ctx coreCfg providers
     putStrLn $ "Starting server at \n " <> "http://localhost:" <> show port
     run port $ app ctx
